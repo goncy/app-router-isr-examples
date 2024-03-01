0fddc347f5fffcdd155364d14749721f7f74aedc
goncy <gonzalo.pozzo4@gmail.com>
2024-03-01 12:02:20 -0300

    Static example

---
+++ src/app/empty-generatestaticparams/[store]/page.tsx
@@ -0,0 +1,13 @@
+export async function generateStaticParams() {
+  return []
+}
+
+export default async function Store({params}: {params: {store: string}}) {
+  const {products} = await fetch(`https://dummyjson.com/products?store=${params.store}`).then(res => res.json() as Promise<{products: unknown[]}>);
+  
+  console.log(Date.now())
+
+  return (
+    <div>Products: {products.length}</div>
+  );
+}
